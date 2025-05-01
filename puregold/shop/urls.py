f156f4bb0e5fff5c2196f9fda75e17ec2f5e3663
Abdulmanan <mohammadabdulmanan24@gmail.com>
2025-05-02 07:07:29 +0800

    Lets proceed to customer

--- puregold/shop/urls.py
+++ puregold/shop/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
 from .views import *
 
+app_name = 'shop'
+
 urlpatterns = [
     # Main
+    path('',shop,name="shop"),
+    path('login/',login,name="login"),
+    path('register/',register,name="register"),
 ] 
--- puregold/shop/views.py
+++ puregold/shop/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from inventory.models import *
 
 # Create your views here.
+
+def shop(request):
+    context = {
+        'stocks': Stock.objects.all()
+    }
+    return render(request,'shop.html', context)
+
+def login(request):
+    return render(request,'content/login.html')
+
+def register(request):
+    return render(request,'content/register.html')
